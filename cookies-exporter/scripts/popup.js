3f19cedf3f3f7a8a9120de337a892a6b74a05216
aa65535 <aa65535@live.com>
2014-10-26 22:12:58 +0800

    rename variable

--- cookies-exporter/scripts/popup.js
+++ cookies-exporter/scripts/popup.js
@@ -3,10 +3,10 @@ chrome.tabs.getSelected(null, function(tab) {
 	chrome.cookies.getAll({url: tab.url}, function(cookies) {
 		var i, comment, content = '';
 		comment = '<pre>\n'
-			+ '# Cookies for domains related to <b>' + escapeForPre(domain) + '</b>.\n'
+			+ '# Cookies for domains related to <b>' + htmlspecialchars(domain) + '</b>.\n'
 			+ '# This content may be pasted into a cookies.txt file and used by wget or curl\n'
-			+ '# Example:  wget -x <b>--load-cookies cookies.txt</b> ' + escapeForPre(tab.url) + '\n'
-			+ '# Example:  curl <b>-b cookies.txt</b> ' + escapeForPre(tab.url) + '\n'
+			+ '# Example:  wget -x <b>--load-cookies cookies.txt</b> ' + htmlspecialchars(tab.url) + '\n'
+			+ '# Example:  curl <b>-b cookies.txt</b> ' + htmlspecialchars(tab.url) + '\n'
 			+ '#\n';
 		for (i in cookies) {
 			content += cookies[i].domain
@@ -24,15 +24,15 @@ chrome.tabs.getSelected(null, function(tab) {
 				+ cookies[i].value
 				+ '\n';
 		}
-		document.write(comment + escapeForPre(content) + '</pre>');
+		document.write(comment + htmlspecialchars(content) + '</pre>');
 	});
 });
 
-function escapeForPre(text) {
-	return String(text)
+function htmlspecialchars(str) {
+	return String(str)
 		.replace(/&/g, '&amp;')
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;')
 		.replace(/"/g, '&quot;')
-		.replace(/'/g, '&#039;');
+		.replace(/'/g, '&#039;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;');
 }

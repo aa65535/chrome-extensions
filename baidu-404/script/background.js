9295099af34867c646e65c67685e745c517feade
Jian Chang <aa65535@live.com>
2016-10-23 11:51:16 +0800

    update baidu-404

--- baidu-404/script/background.js
+++ baidu-404/script/background.js
@@ -1,14 +1,16 @@
 function modifyHeader(headers, url) {
     var i, len, reg, Request = {};
 
-    reg = /http:\/\/.*\.baidu\.com\/.*/;
+    reg = /https?:\/\/.*\.baidu\.com\/.*/;
 
     if (reg.test(url)) {
         len = headers.length;
 
         for (i = 0; i < len; i++) {
             if (headers[i].name == 'Referer') {
-                headers[i].value = url;
+                if (!reg.test(headers[i].value)) {
+                    headers[i].value = url;
+                }
                 break;
             }
         }
